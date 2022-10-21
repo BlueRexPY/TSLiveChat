e2bc00dea3ce6ec9d4b2b3f89e577403de4d6ff3
Ruslan <66295121+BlueRexPY@users.noreply.github.com>
2022-10-21 16:35:01 +0200

    Add files via upload

--- src/Styles/them.js
+++ src/Styles/them.js
@@ -1,26 +1,26 @@
-import { createTheme } from '@mui/material/styles';
+import { createTheme } from "@mui/material/styles";
 
 export const theme = createTheme({
-    palette: {
-      mode: 'light',
-      background: {
-        paper: '#252331',
-        primary: '#211E2D',
-        secondary: '#2161FD',
-      },
-      text: {
-        primary: '#fff',
-        secondary: '#fff',
-        dark: '#4a4364',
-      },
-      primary: {
-        main: "#252331",
-      },
-      secondary: {
-        main: '#2161FD',
-      },
-      error: {
-        main:'#0CF25D',
-      },
+  palette: {
+    mode: "light",
+    background: {
+      paper: "#252331",
+      primary: "#211E2D",
+      secondary: "#2161FD",
     },
-  });
+    text: {
+      primary: "#fff",
+      secondary: "#fff",
+      dark: "#4a4364",
+    },
+    primary: {
+      main: "#252331",
+    },
+    secondary: {
+      main: "#2161FD",
+    },
+    error: {
+      main: "#0CF25D",
+    },
+  },
+});
